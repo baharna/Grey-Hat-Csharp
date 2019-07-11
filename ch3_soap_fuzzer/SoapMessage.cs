96010ba914869aea32b93e78501cc03ea6174598
baharna <travis.s.whitney@gmail.com>
2019-07-10 20:20:27 -0400

    Create SoapMessage.cs

---
+++ ch3_soap_fuzzer/SoapMessage.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace soap_fuzzer
+{
+    public class SoapMessage
+    {
+        public SoapMessage(XmlNode node)
+        {
+            this.Name = node.Attributes["name"].Value;
+            this.Parts = new List<SoapMessagePart>();
+            if (node.HasChildNodes)
+            {
+                foreach (XmlNode part in node.ChildNodes)
+                    this.Parts.Add(new SoapMessagePart(part));
+            }
+        }
+
+        public string Name { get; set; }
+        public List<SoapMessagePart> Parts { get; set; }
+    }
+}
