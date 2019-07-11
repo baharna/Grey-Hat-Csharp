e655581d8ae905cab098c6e2399b08d755a92f88
baharna <travis.s.whitney@gmail.com>
2019-07-10 20:21:28 -0400

    Create SoapOperation.cs

---
+++ ch3_soap_fuzzer/SoapOperation.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace soap_fuzzer
+{
+    public class SoapOperation
+    {
+        public SoapOperation(XmlNode op)
+        {
+            this.Name = op.Attributes["name"].Value;
+            foreach (XmlNode message in op.ChildNodes)
+            {
+                if (message.Name.EndsWith("input"))
+                    this.Input = message.Attributes["message"].Value;
+                else if (message.Name.EndsWith("output"))
+                    this.Output = message.Attributes["message"].Value;
+            }
+        }
+
+        public string Name { get; set; }
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+}
